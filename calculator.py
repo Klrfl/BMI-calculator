e5565734c5eac5faadd9c1b665a967aaa8c04061
Efraim Munthe <efraimmunthe101@gmail.com>
2022-10-27 12:07:30 +0700

    kalkulator awal

---
+++ calculator.py
@@ -0,0 +1,27 @@
+# input
+
+nama = input("namamu siapa? ")
+berat = float(input("masukkan berat (kg): "))
+tinggi = float(input("masukkan tinggi (cm): ")) / 100
+
+bmi = round(berat / tinggi**2, 2)
+
+print(f"\n{nama}, BMI kamu adalah {bmi}.")
+
+if bmi < 16:
+  print("kamu kekurangan berat badan. Banget.")
+elif 16 < bmi < 16.9:
+  print("kamu sedikit kekurangan badan.")
+elif 17 < bmi < 18.4:
+  print("Dikit lagi berat badanmu normal. Makan yang banyak ya.")
+
+elif 18.5 < bmi < 24.9:
+  print("BMI kamu normal. Pertahankan ya.")
+elif 25 < bmi < 29.9:
+  print("kamu sedikit kelebihan berat badan.")
+elif 30 < bmi < 34.9:
+  print("kamu masuk obesitas kelas 1.")
+elif 35 < bmi < 39.9:
+  print("kamu masuk obesitas kelas 2.")
+elif bmi > 40:
+  print("kamu masuk obesitas kelas 2.")
